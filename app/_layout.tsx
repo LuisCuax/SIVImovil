51e1de7ed64a61b0d280e6b45488d928ac74b595
LuisCuax <diegoarroyo911@gmial.com>
2025-12-05 00:08:47 -0600

    Set login screen as initial app route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,10 @@ import { RondinProvider } from '../contexts/RondinContext';
 export default function RootLayout() {
   return (
     <RondinProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-    </RondinProvider>
+              <Stack>
+                <Stack.Screen name="login" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="login" options={{ headerShown: false }} />
+              </Stack>    </RondinProvider>
   );
 }
